1425ce01245e8e895d0d3ea47ca18daca2d5bb71
Abhishek <abhishek.ranjan@cloudera.com>
2024-07-25 20:24:19 +0530

    Changed Input output to pydantic

--- eval/mlflow_driver.py
+++ eval/mlflow_driver.py
@@ -3,9 +3,16 @@ from eval.mlflow_logger import ModelLogger
 from eval.data_loader import Dataloader
 from eval.make_peft_pipeline import fetch_pipeline
 import pandas as pd
+from eval.eval_job import StartEvaluationRequest, EvaluationResponse
 
 
-def driver(dataset_name, base_model_name, adapter_name, device="gpu"):
+def driver(StartEvaluationRequest):
+        
+    dataset_name = StartEvaluationRequest.dataset_name 
+    base_model_name = StartEvaluationRequest.base_model_name 
+    adapter_name = StartEvaluationRequest.adapter_path 
+    device="gpu"
+
     dataloader = Dataloader()
     logger = ModelLogger()
     evaluator = ModelEvaluator()
@@ -22,11 +29,7 @@ def driver(dataset_name, base_model_name, adapter_name, device="gpu"):
     results = evaluator.evaluate_model(model_info, eval_dataset, eval_column_name)
 
     results_df = pd.DataFrame(results.tables['eval_results_table'])
-    return results.metrics, results_df
+    response = EvaluationResponse(metrics = results.metrics, csv = results_df)
+    return response
 
 
-if __name__ == "__main__":
-    results, results_df = driver('philschmid/sql-create-context-copy', 'bigscience/bloom-1b1',
-                                 'data/adapters/bloom1b1-lora-sql')
-    print(results)
-    print(results_df)
