a75a749c6eb47cc4d39cdf880a8a73e90399b88a
Jason Everett <jev@Jasons-MacBook-Pro.local>
2024-08-18 07:34:24 -0400

    Auto add FTS client IP/port to project env

--- bin/start-grpc-server.py
+++ bin/start-grpc-server.py
@@ -7,6 +7,10 @@ from ft.service import FineTuningStudioApp
 from ft.consts import DEFAULT_FTS_GRPC_PORT
 from multiprocessing import Process
 import socket 
+import cmlapi
+import os
+import json
+from typing import Dict
 
 def start_server(blocking: bool = False):
     port = DEFAULT_FTS_GRPC_PORT
@@ -20,7 +24,33 @@ def start_server(blocking: bool = False):
         server.wait_for_termination()
 
 
-# Start the server up. If this command fails (if the port is already
-# in use), the application script bin/start-app-script.sh will continue
-# to run and the error will exit gracefully.
-start_server(blocking=True)
+
+if __name__ == "__main__":
+    
+    # Make the fine tuning studio IP address and port available as project-level 
+    # environment variables, so we can instantiate clients from anywhere 
+    # within the project.
+    
+    cml = cmlapi.default_client()
+    
+    project_id = os.getenv("CDSW_PROJECT_ID")
+    grpc_address = os.getenv("CDSW_IP_ADDRESS")
+    grpc_port = DEFAULT_FTS_GRPC_PORT
+    
+    proj: cmlapi.Project = cml.get_project(project_id)
+    proj_env: Dict = json.loads(proj.environment)
+    proj_env.update({
+        "FINE_TUNING_SERVICE_IP": grpc_address,
+        "FINE_TUNING_SERVICE_PORT": grpc_port
+    })
+    
+    updated_project: cmlapi.Project = cmlapi.Project(
+        environment= json.dumps(proj_env)
+    )
+    out: cmlapi.Project = cml.update_project(updated_project, project_id=project_id)
+    print(out.environment)
+
+    # Start the server up. If this command fails (if the port is already
+    # in use), the application script bin/start-app-script.sh will continue
+    # to run and the error will exit gracefully.
+    start_server(blocking=True)
