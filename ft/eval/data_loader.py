cf51a1a8771b91a27768e5a0adbf0f007ceaef0d
Abhishek <abhishek.ranjan@cloudera.com>
2024-08-20 15:10:24 +0530

    Minor bug fix in loading dataset

--- ft/eval/data_loader.py
+++ ft/eval/data_loader.py
@@ -7,7 +7,7 @@ from ft.client import FineTuningStudioClient
 from ft.api import *
 
 
-class Dataloader():
+class Dataloader:
 
     @staticmethod
     def fetch_evaluation_dataset(dataset_id: str, total_examples: int = 50, client: FineTuningStudioClient = None):
@@ -16,9 +16,10 @@ class Dataloader():
             # return this as error in UI
             raise ValueError(f"Dataset with id of {dataset_id} not found in the available datasets.")
         # TODO: remove hardcoded dependency on HF name (allow for project-relative dataset loading)
-        dataset = load_dataset(dataset.huggingface_name, cache_dir=get_unique_cache_dir())
-        eval_df = pd.DataFrame(dataset["train"])
-        eval_column_name, template_function = fetch_eval_column_name_and_merge_function(dataset.huggingface_name)
+        dataset_hf_name = dataset.huggingface_name
+        loaded_dataset = load_dataset(dataset_hf_name, cache_dir=get_unique_cache_dir())
+        eval_df = pd.DataFrame(loaded_dataset["train"])
+        eval_column_name, template_function = fetch_eval_column_name_and_merge_function(dataset_hf_name)
         eval_df = eval_df.sample(n=total_examples)
         eval_df['model_input'] = eval_df.apply(lambda x: template_function(x), axis=1)
         print(eval_df)
