1a3e7a14fbcd746b46018d20567957f3174a7934
Jason Everett <jev@cloudera.com>
2024-08-26 14:28:19 +0000

    Script to compile SQLite3 CLI into project

---
+++ bin/get-sqlite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~
+rm -rf sqlite3/
+mkdir -p sqlite3/
+cd sqlite3/
+wget https://www.sqlite.org/2024/sqlite-autoconf-3460100.tar.gz
+tar -xzvf sqlite-autoconf-3460100.tar.gz
+cd sqlite-autoconf-3460100
+./configure
+make -j
+echo "export PATH=~/sqlite3/sqlite-autoconf-3460100/:$PATH" >> ~/.bashrc
