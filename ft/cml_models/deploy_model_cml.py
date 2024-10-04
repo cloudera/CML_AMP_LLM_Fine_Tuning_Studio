e55cf04fff23c286486352bdc51668c9a4980eec
Jason Everett <jev@cloudera.com>
2024-10-04 01:20:52 +0000

    get runtime identifier

--- ft/cml_models/deploy_model_cml.py
+++ ft/cml_models/deploy_model_cml.py
@@ -1,8 +1,27 @@
 import cmlapi
 import os
 import time
+from cmlapi import CMLServiceApi
 
-FILEPATH="ft/cml_models/predict.py"
+FILEPATH = "ft/cml_models/predict.py"
+
+
+def get_inference_runtime_identifier(cml: CMLServiceApi) -> str:
+    """
+    Get a runtime ID to be used for inference in CML models. For now, we will use
+    the same runtime ID as the fine tuning training jobs, since that runtime ID has
+    all components necessary for GPU inference on CUDA enabled devices.
+    """
+    project_id = os.getenv("CDSW_PROJECT_ID")
+    base_job_name = "Finetuning_Base_Job"
+
+    ft_base_job_id = cml.list_jobs(project_id,
+                                   search_filter='{"name":"%s"}' % base_job_name).jobs[0].id
+    template_job = cml.get_job(
+        project_id=project_id,
+        job_id=ft_base_job_id
+    )
+    return template_job.runtime_identifier
 
 
 def deploy_model():
@@ -12,7 +31,13 @@ def deploy_model():
     project: cmlapi.Project = client.get_project(project_id)
     model_body = cmlapi.CreateModelRequest(project_id=project.id, name="Demo Model", description="A simple model")
     model = client.create_model(model_body, project.id)
-    model_build_body = cmlapi.CreateModelBuildRequest(project_id=project.id, model_id=model.id, file_path=FILEPATH, function_name="api_wrapper", kernel="python3")
+    model_build_body = cmlapi.CreateModelBuildRequest(
+        project_id=project.id,
+        model_id=model.id,
+        file_path=FILEPATH,
+        function_name="api_wrapper",
+        runtime_identifier=get_inference_runtime_identifier(client),
+        kernel="python3")
     model_build = client.create_model_build(model_build_body, project.id, model.id)
     while model_build.status not in ["built", "build failed"]:
         print("waiting for model to build...")
@@ -23,7 +48,8 @@ def deploy_model():
         print("model build failed, see UI for more information")
         raise Exception.exit(1)
     print("model built successfully!")
-    model_deployment_body = cmlapi.CreateModelDeploymentRequest(project_id=project.id, model_id=model.id, build_id=model_build.id)
+    model_deployment_body = cmlapi.CreateModelDeploymentRequest(
+        project_id=project.id, model_id=model.id, build_id=model_build.id)
     model_deployment = client.create_model_deployment(model_deployment_body, project.id, model.id, model_build.id)
     while model_deployment.status not in ["stopped", "failed", "deployed"]:
         print("waiting for model to deploy...")
--- ft/cml_models/predict.py
+++ ft/cml_models/predict.py
@@ -1,4 +1,3 @@
-import cmlapi
 from ft.eval.mlflow_pyfunc import MLFlowTransformers
 import torch
 
@@ -15,43 +14,46 @@ class SingletonModelFetcher:
             cls._instance.initialize_model()
         return cls._instance
 
-    def initialize_model(self,base_model_name, peft_model_name):
+    def initialize_model(self, base_model_name, peft_model_name):
         # Load model from disk or initialize it if necessary
         # This assumes that the model is already trained and saved in a file
-        model,tokenizer = mlt.get_peft_model_and_tokenizer(base_model_name, peft_model_name)
+        model, tokenizer = mlt.get_peft_model_and_tokenizer(base_model_name, peft_model_name)
         return model, tokenizer
+
 
 modelFetcher = SingletonModelFetcher()
 model, tokenizer = modelFetcher.initialize_model("huggingface/gpt2", "adapter/gpt2-ticketing")
 
 
 def opt_args_value(args, arg_name, default):
-  if arg_name in args.keys():
-    return args[arg_name]
-  else:
-    return default
+    if arg_name in args.keys():
+        return args[arg_name]
+    else:
+        return default
+
 
 def generate(prompt, max_new_tokens=128, temperature=0.7, repetition_penalty=1.0, num_beams=1, top_p=1.0, top_k=0):
     device = torch.device('cuda')
     input_tokens = tokenizer(prompt, return_tensors='pt').to(device)
     output_tokens = model.generate(**input_tokens,
-                                    max_new_tokens=max_new_tokens,
-                                    repetition_penalty=repetition_penalty,
-                                    temperature=temperature,
-                                    num_beams=num_beams,
-                                    top_p=top_p,
-                                    top_k=top_k)
+                                   max_new_tokens=max_new_tokens,
+                                   repetition_penalty=repetition_penalty,
+                                   temperature=temperature,
+                                   num_beams=num_beams,
+                                   top_p=top_p,
+                                   top_k=top_k)
     prompt_length = len(prompt)
     return tokenizer.decode(output_tokens[0], skip_special_tokens=True)[prompt_length:]
 
+
 def api_wrapper(args):
-  # Pick up args from model api
-  prompt = args["prompt"]
-  temperature = float(opt_args_value(args, "temperature", 70))
-  max_new_tokens = float(opt_args_value(args, "max_new_tokens", 50))
-  top_p = float(opt_args_value(args, "top_p", 1.0))
-  top_k = int(opt_args_value(args, "top_k", 0))
-  repetition_penalty = float(opt_args_value(args, "repetition_penalty", 1.0))
-  num_beams = int(opt_args_value(args, "num_beams", 1))
-  
-  return generate(prompt, max_new_tokens, temperature, repetition_penalty, num_beams, top_p, top_k)
+    # Pick up args from model api
+    prompt = args["prompt"]
+    temperature = float(opt_args_value(args, "temperature", 70))
+    max_new_tokens = float(opt_args_value(args, "max_new_tokens", 50))
+    top_p = float(opt_args_value(args, "top_p", 1.0))
+    top_k = int(opt_args_value(args, "top_k", 0))
+    repetition_penalty = float(opt_args_value(args, "repetition_penalty", 1.0))
+    num_beams = int(opt_args_value(args, "num_beams", 1))
+
+    return generate(prompt, max_new_tokens, temperature, repetition_penalty, num_beams, top_p, top_k)
