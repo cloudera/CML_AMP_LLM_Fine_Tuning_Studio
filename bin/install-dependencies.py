f14eb5212aa2cd5f2ff511a7ce7eb63408960226
Jason Everett <jev@cloudera.com>
2024-10-25 08:34:07 -0400

    Update install-dependencies.py

--- bin/install-dependencies.py
+++ bin/install-dependencies.py
@@ -1,1 +1,1 @@
-!pip3 install --upgrade pip && pip3 install --no-cache-dir -r requirements.txt && pip3 install -e .
+!pip3 install --upgrade pip && pip3 install --no-cache-dir --use-feature=fast-deps -r requirements.txt --jobs 4 && pip3 install -e .
