902bdecfa0098b443f2bfaf5effc0f5551bc5710
Abhishek Ranjan <abhishek.ranjan@cloudera.com>
2025-01-22 17:57:42 +0530

    Bug fix for initialize defaults (#25)

--- bin/initialize-project-defaults-uv.py
+++ bin/initialize-project-defaults-uv.py
@@ -1,1 +1,6 @@
-!uv run bin/initialize-project-defaults.py
+import os
+
+if os.getenv("IS_COMPOSABLE", "") != "":
+    !uv run /home/cdsw/fine-tuning-studio/bin/initialize-project-defaults.py
+else:
+    !uv run /home/cdsw/bin/initialize-project-defaults.py
