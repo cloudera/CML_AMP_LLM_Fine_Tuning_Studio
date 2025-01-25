8b096a6c610f5b5190e4c96b809b7d8714bc0979
Abhishek <abhishek.ranjan@cloudera.com>
2025-01-25 12:32:04 +0530

    bug fixes

--- bin/run-app.py
+++ bin/run-app.py
@@ -1,6 +1,9 @@
 import subprocess
 import os 
 
+if os.getenv("IS_COMPOSABLE", "") != "":
+  os.chdir("/home/cdsw/fine-tuning-studio")
+
 CDSW_APP_PORT = os.environ.get("CDSW_APP_PORT")
 out = subprocess.run([f"bash ./bin/start-app-script.sh {CDSW_APP_PORT}"], shell=True, check=True)
 print(out)
